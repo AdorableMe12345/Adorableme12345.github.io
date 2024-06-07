9b33b4ab62074ec0cae718322e5789c71dd7876f
AdorableMe12345 <arambulopearlandrie@gmail.com>
2024-06-07 21:04:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-const btnDisLike1 = document.getElementById("btnDisLike1")
-const btnDisLike2 = document.getElementById("btnDisLike2")
+const btnDislike1 = document.getElementById("btnDislike1")
+const btnDislike2 = document.getElementById("btnDislike2")
 
-const countDisLikes1 = document.getElementById("countDisLikes1")
-const countDisLikes2 = document.getElementById("countDisLikes2")
+const countDislikes1 = document.getElementById("countDislikes1")
+const countDislikes2 = document.getElementById("countDislikes2")
 
 function hitLikes1() {
    let totalLikes1 = parseInt(countLikes1.value) + 1
