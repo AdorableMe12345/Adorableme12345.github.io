7c67840d4bb8c4ccda4c3ebca08e67ac89e63e29
AdorableMe12345 <arambulopearlandrie@gmail.com>
2024-06-08 11:51:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
 
 commentbox.textContent += comment.value.toString() + "\n"
-comment.value = "";
+comment.value = " ";
 }
 submit.addEventListener("click", submitComment)
 
